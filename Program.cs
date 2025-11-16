e0a3d3508172ba8ced1b1f1e522a0560bc9d0efb
Jason Wright <jason@thought.net>
2025-11-16 14:24:42 -0700

    add infrastructure for integrating quantum solver backend

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+Console.WriteLine($"BASEURL FOR QUANTUM: {QuantumSolverSettings.BaseURL}");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
