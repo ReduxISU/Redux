394b33293cae6dc4cb5fbf0a04da828f61f4d5f5
hilleri2 <hilleri2@isu.edu>
2025-12-16 14:34:41 -0700

    (mostly) Fixed instance and parsing errors
    Fixed the default instance to be a correct input for the problem. Also fixed the parsing to work as expected to give the values back from the problem instance. The parser produces an error at some point which I haven't worked out, but the outputs from it are still good (and it was based on the weighted cut implementation, which gives an error of its own). So it was good enough for me for now.

--- Problems/NPComplete/NPC_MAXCUT/MAXCUT_Class.cs
+++ Problems/NPComplete/NPC_MAXCUT/MAXCUT_Class.cs
@@ -17,12 +17,14 @@ class MAXCUT : IGraphProblem<MaxCutSolver, MaxCutVerifier, MaxCutVisualization, 
     
     public string source {get;} = "Karp, Richard M. Reducibility among combinatorial problems. Complexity of computer computations. Springer, Boston, MA, 1972. 85-103.";
     public string sourceLink { get; } = "https://cgi.di.uoa.gr/~sgk/teaching/grad/handouts/karp.pdf";
-    private static string _defaultInstance = "({1,2,3,4,5},{{2,1},{1,3},{2,3},{3,5},{2,4},{4,5}})";
+    // private static string _defaultInstance = "({1,2,3,4,5},{{2,1},{1,3},{2,3},{3,5},{2,4},{4,5}})";
+    public static string _defaultInstance { get; } = "({1,2,3,4,5},{({2,1},5),({1,3},4),({2,3},2),({3,5},1),({2,4},4),({4,5},2)})";
     public string defaultInstance { get; } = _defaultInstance;
     public string instance {get;set;} = string.Empty;
     
     private List<string> _nodes = new List<string>();
-    private List<KeyValuePair<string, string>> _edges = new List<KeyValuePair<string, string>>();
+    // private List<KeyValuePair<string, string>> _edges = new List<KeyValuePair<string, string>>();
+    private List<(string source, string destination, int weight)> _edges = new List<(string source, string destination, int weight)>();
     private int _K;
     public MaxCutSolver defaultSolver {get;} = new MaxCutSolver();
     public MaxCutVerifier defaultVerifier {get;} = new MaxCutVerifier();
@@ -41,11 +43,23 @@ class MAXCUT : IGraphProblem<MaxCutSolver, MaxCutVerifier, MaxCutVisualization, 
             _nodes = value;
         }
     }
-    public List<KeyValuePair<string, string>> edges {
-        get {
+    // public List<KeyValuePair<string, string>> edges {
+    //     get {
+    //         return _edges;
+    //     }
+    //     set {
+    //         _edges = value;
+    //     }
+    // }
+
+    public List<(string source, string destination, int weight)> edges
+    {
+        get
+        {
             return _edges;
         }
-        set {
+        set
+        {
             _edges = value;
         }
     }
@@ -57,14 +71,38 @@ class MAXCUT : IGraphProblem<MaxCutSolver, MaxCutVerifier, MaxCutVisualization, 
     public MAXCUT(string GInput) {
         instance = GInput;
 
-        StringParser maxcut = new("{(N,E) | N is set, E subset N unorderedcross N}");
-        maxcut.parse(GInput);
-        nodes = maxcut["N"].ToList().Select(node => node.ToString()).ToList();
-        edges = maxcut["E"].ToList().Select(edge =>
+        // StringParser maxcut = new("{(N,E) | N is set, E subset N unorderedcross N}");
+        // maxcut.parse(GInput);
+        // nodes = maxcut["N"].ToList().Select(node => node.ToString()).ToList();
+        // edges = maxcut["E"].ToList().Select(edge =>
+        // {
+        //     List<UtilCollection> cast = edge.ToList();
+        //     return new KeyValuePair<string, string>(cast[0].ToString(), cast[1].ToString());
+        // }).ToList();
+        // graph = new UtilCollectionGraph(maxcut["N"], maxcut["E"]);
+
+        StringParser weightedCut = new("{(N,E) | N is set, E subset {(e, w) | e is N unorderedcross N, w is int}}");
+        // StringParser weightedCut = new("{(N,E) | N is set, E subset {(e, w) | e subset {(j,k) | j is N unorderedcross N, k is N unorderedcross N}, w is int}}");
+        Console.WriteLine($"Parsing instance: {instance}");
+        weightedCut.parse(GInput);
+        nodes = weightedCut["N"].ToList().Select(node => node.ToString()).ToList();
+        Console.WriteLine($"Parsed Nodes:");
+        foreach (var node in nodes)
         {
-            List<UtilCollection> cast = edge.ToList();
-            return new KeyValuePair<string, string>(cast[0].ToString(), cast[1].ToString());
+            Console.WriteLine($"{node}");
+        }
+        edges = weightedCut["E"].ToList().Select(edge =>
+        {
+            List<UtilCollection> cast = edge[0].ToList();
+            return (cast[0].ToString(), cast[1].ToString(), int.Parse(edge[1].ToString()));
         }).ToList();
-        graph = new UtilCollectionGraph(maxcut["N"], maxcut["E"]);
+        Console.WriteLine($"Parsed Edges:");
+        foreach (var edge in edges)
+        {
+            Console.WriteLine($"{edge}");
+        }
+        // _K = int.Parse(weightedCut["K"].ToString());
+
+        graph = new UtilCollectionGraph(weightedCut["N"], weightedCut["E"]);
     }
 }
