a4300df1a634d9f8ef2c7057dc87901c8ebdb550
Andrija-Sevaljevic <andrijasevaljevic@isu.edu>
2025-12-22 15:18:01 -0700

    New ruduction plus exact cover visualization

--- Problems/NPComplete/NPC_EXACTCOVER/Visualizations/ExactCoverDefaultVisualization.cs
+++ Problems/NPComplete/NPC_EXACTCOVER/Visualizations/ExactCoverDefaultVisualization.cs
@@ -8,23 +8,26 @@ namespace API.Problems.NPComplete.NPC_EXACTCOVER.Visualizers;
 
 class ExactCoverDefaultVisualization : IVisualization<EXACTCOVER>
 {
-    public string visualizationName { get; } = "Clique Visualization";
-    public string visualizationDefinition { get; } = "This is a default visualization for clique";
+    public string visualizationName { get; } = "Exact Cover Visualization";
+    public string visualizationDefinition { get; } = "This is a default visualization for Exact Cover";
     public string source { get; } = "";
-    public string[] contributors { get; } = { "Russell Phillips" };
-    public string visualizationType { get; } = "Set";
+    public string[] contributors { get; } = { "Russell Phillips, Andrija Sevaljevic" };
+    public string visualizationType { get; } = "Set D3";
 
     // --- Methods Including Constructors ---
     public ExactCoverDefaultVisualization()
     {
 
     }
-    public API_JSON visualize(EXACTCOVER clique)
+    public API_JSON visualize(EXACTCOVER exactCover)
     {
-        return new API_SET(new UtilCollection(clique.instance));
+        return new API_SET(new UtilCollection(exactCover.instance));
     }
-    public API_JSON SolvedVisualization(EXACTCOVER clique, string solution)
+    public API_JSON SolvedVisualization(EXACTCOVER exactCover, string solution)
     {
-        return new API_SET(new UtilCollection(clique.instance));
+        //return new API_SET(new UtilCollection(exactCover.instance));
+        API_SET ec = new API_SET(new UtilCollection(exactCover.instance));
+        
+        return new API_SET(new UtilCollection(exactCover.instance));
     }
 }
