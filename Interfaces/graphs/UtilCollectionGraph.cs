8931b0232d195fa1467780258a41972771d09726
Jetison333 <Jetison333@hotmail.com>
2026-01-14 16:15:07 -0700

    Add support for self edges in undirected graphs for the API in UtilCollectionGraph

--- Interfaces/graphs/UtilCollectionGraph.cs
+++ Interfaces/graphs/UtilCollectionGraph.cs
@@ -106,6 +106,10 @@ class UtilCollectionGraph : Graph
                 edges = EdgeList.Select(edge =>
                 {
                     List<UtilCollection> cast = edge[0].ToList();
+                    if (cast.Count == 1) // self edge is a set with only one element, since {1,1} = {1}
+                    {
+                        return new KeyValuePair<string, string>(cast[0].ToString(), cast[0].ToString());
+                    }
                     return new KeyValuePair<string, string>(cast[0].ToString(), cast[1].ToString());
                 }).ToList();
 
@@ -123,6 +127,10 @@ class UtilCollectionGraph : Graph
                 edges = EdgeList.Select(edge =>
                 {
                     List<UtilCollection> cast = edge.ToList();
+                    if (cast.Count == 1) // self edge is a set with only one element, since {1,1} = {1}
+                    {
+                        return new KeyValuePair<string, string>(cast[0].ToString(), cast[0].ToString());
+                    }
                     return new KeyValuePair<string, string>(cast[0].ToString(), cast[1].ToString());
                 }).ToList();
 
