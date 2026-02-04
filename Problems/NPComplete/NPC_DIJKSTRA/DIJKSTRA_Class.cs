c9394397eb1021725d4343bd39ec897664bb575f
RajNilkar <rajitnilkar28@gmail.com>
2026-02-04 08:35:02 -0700

    Updated Problem Definition and Instance

--- Problems/NPComplete/NPC_DIJKSTRA/DIJKSTRA_Class.cs
+++ Problems/NPComplete/NPC_DIJKSTRA/DIJKSTRA_Class.cs
@@ -4,7 +4,7 @@ using API.Problems.NPComplete.NPC_DIJKSTRA.Verifiers;
 using API.Problems.NPComplete.NPC_DIJKSTRA.Visualizations;
 using SPADE;
 
-namespace API.Problems.NPComplete.NPC_HAMILTONIAN;
+namespace API.Problems.NPComplete.NPC_Dijkstra;
 
 class DIJKSTRA : IGraphProblem<DijkstraBruteForce, DijkstraVerifier, DijkstraDefaultVisualization, UtilCollectionGraph>
 {
@@ -13,10 +13,10 @@ class DIJKSTRA : IGraphProblem<DijkstraBruteForce, DijkstraVerifier, DijkstraDef
     public string problemName { get; } = "Dijkstra's algorithm";
     public string problemLink { get; } = "https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm";
     public string formalDefinition { get; } = "Dijkstra's algorithm = {[G, V, E, l] | \"G = (V, E) whose edge lengths l are positive integers\" }"; //TODO figure out what it's supposed to be
-    public string problemDefinition { get; } = "Dijkstra’s Shortest Path Problem is the problem of determining the shortest path distances from a given source vertex to all other vertices in a weighted graph with nonnegative edge weights.";
+    public string problemDefinition { get; } = "Dijkstra’s Shortest Path Problem is the problem of determining the shortest path distances from a given source vertex to all other vertices in a directed graph with strictly positive edge weights.";
     public string source { get; } = "Dijkstra, E. W. A note on two problems in connexion with graphs. Numerische Mathematik 1, 1959, 269–271.";
     public string sourceLink { get; } = "https://ir.cwi.nl/pub/9256/9256D.pdf";
-    private static string _defaultInstance = "({1,2,3,4,5},{{2,1},{1,3},{2,3},{3,5},{2,4},{4,5}})";
+    private static string _defaultInstance = "({1,2,3,4,5},{{2,1,4},{1,3,2},{2,3,1},{3,5,7},{2,4,3},{4,5,9}})";
     public string defaultInstance { get; } = _defaultInstance;
     public string instance { get; set; } = string.Empty;
 
@@ -27,7 +27,7 @@ class DIJKSTRA : IGraphProblem<DijkstraBruteForce, DijkstraVerifier, DijkstraDef
     public DijkstraVerifier defaultVerifier { get; } = new DijkstraVerifier();
     public DijkstraDefaultVisualization defaultVisualization { get; } = new DijkstraDefaultVisualization();
     public UtilCollectionGraph graph { get; set; }
-    public string[] contributors { get; } = { "Tiger Sant", "Malaya Witt" };
+    public string[] contributors { get; } = { "Tiger Sant", "Malaya Witt, Rajit Nilkar, Scott Barfuss" };
 
     // --- Properties ---
     public List<string> nodes
