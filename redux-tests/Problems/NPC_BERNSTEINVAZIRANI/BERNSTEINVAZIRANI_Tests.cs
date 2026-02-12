7b3f2edb33dc492f6a33434db349e630433202a8
Corbin Hay <heycorbinhay@gmail.com>
2026-02-12 14:41:29 -0700

    attempting to fix the BERNSTEINVAZIRANT unit tests

--- redux-tests/Problems/NPC_BERNSTEINVAZIRANI/BERNSTEINVAZIRANI_Tests.cs
+++ redux-tests/Problems/NPC_BERNSTEINVAZIRANI/BERNSTEINVAZIRANI_Tests.cs
@@ -9,22 +9,22 @@ public class BERNSTEINVAZIRANI_tests {
     [Fact]
     public void BERNSTEINVAZIRANI_Default_Instantiation() {
         var problem = new BERNSTEINVAZIRANI();
-        Assert.Equal("(0,1,0,1,1,0,1,0)", problem.instance);
-        Assert.Equal("(0,1,0,1,1,0,1,0)", problem.defaultInstance);
+        Assert.Equal("(3,(0,1,0,1,1,0,1,0))", problem.instance);
+        Assert.Equal("(3,(0,1,0,1,1,0,1,0))", problem.defaultInstance);
     } 
 
     [Fact]
     public void BERNSTEINVAZIRANI_Custom_Instantiation() {
-        string instance = "(0,1,0,1,0,1,0,1)";
+        string instance = "(3,(0,1,0,1,1,0,1,0))";
         var problem = new BERNSTEINVAZIRANI(instance);
         Assert.Equal(instance, problem.instance);
     }
 
     [Theory] //tests verifier
-    [InlineData("(0,1,0,1,1,0,1,0)", "101", true)]
-    [InlineData("(0,1,1,0,1,0,0,1)", "111", true)]
-    [InlineData("(0,1,0,1,0,1,0,1)", "001", true)]
-    [InlineData("(0,0,0,0,0,0,0,0)", "000", true)]
+    [InlineData("(3,(0,1,0,1,1,0,1,0))", "101", true)]
+    [InlineData("(3,(0,1,1,0,1,0,0,1))", "111", true)]
+    [InlineData("(3,(0,1,0,1,0,1,0,1))", "001", true)]
+    [InlineData("(3,(0,0,0,0,0,0,0,0))", "000", true)]
     public void BERNSTEINVAZIRANI_verifier(string instance, string certificate, bool expected) {
         var problem = new BERNSTEINVAZIRANI(instance);
         var verifier = new BernsteinVaziraniClassicalVerifier();
@@ -34,10 +34,10 @@ public class BERNSTEINVAZIRANI_tests {
     }
 
     [Theory] //tests solver
-    [InlineData("(0,1,0,1,1,0,1,0)", "101")]
-    [InlineData("(0,1,1,0,1,0,0,1)", "111")]
-    [InlineData("(0,0,0,0,0,0,0,0)", "000")]
-    [InlineData("(0,1,0,1,0,1,0,1)", "001")]
+    [InlineData("(3,(0,1,0,1,1,0,1,0))", "101")]
+    [InlineData("(3,(0,1,1,0,1,0,0,1))", "111")]
+    [InlineData("(3,(0,0,0,0,0,0,0,0))", "000")]
+    [InlineData("(3,(0,1,0,1,0,1,0,1))", "001")]
     public void BERNSTEINVAZIRANI_solver(string instance, string certificate) {
         var problem = new BERNSTEINVAZIRANI(instance);
         var solver = problem.defaultSolver;
