7305db11391c714c4d89c1b392bfe09e77a9849a
akkabakovb <akkabakov.bektur159@gmai.com>
2026-04-07 19:13:39 -0600

    Clarify convex hull problem definition

--- Problems/NPComplete/NPC_CONVEXHULL/CONVEXHULL_Class.cs
+++ Problems/NPComplete/NPC_CONVEXHULL/CONVEXHULL_Class.cs
@@ -11,7 +11,7 @@ class CONVEXHULL : IProblem<ConvexHullSolver, ConvexHullVerifier, ConvexHullVisu
     public string problemName {get;} = "Convex Hull";
     public string problemLink {get;} = "https://en.wikipedia.org/wiki/Convex_hull#Definitions";
     public string formalDefinition { get; } = "{X ⊆ ℝ² | output the extreme points of X forming the convex hull in counterclockwise order}";
-    public string problemDefinition {get;} = "Given a set of points in the plane, compute the smallest convex polygon that contains all the points. The output is typically given as the vertices of the polygon in counterclockwise order.";
+    public string problemDefinition {get;} = "Given a set of points in the plane, compute the smallest convex polygon that contains all the points. The output is typically given as the vertices of the polygon in clockwise order.";
     public string source {get;} = "de Berg, M.; van Kreveld, M.; Overmars, Mark; Schwarzkopf, O. (2008), Computational Geometry: Algorithms and Applications (3rd ed.), Springer";
     public string sourceLink {get;} = "https://doi.org/10.1007/978-3-540-77974-2_1";
     private static readonly string _defaultInstance = "{(-0.49429993857731547,-0.14539088193288174),(0.7674622377407927,-0.21537444528240846),(-0.9308276577586132,-0.1423800479224624),(-0.6984449872706371,0.3857380723376367),(-0.27394905790800017,-0.7488048223660126),(0.6681904385614421,-0.11341930745315643),(0.6077591838324792,0.5288040272918157),(0.32371432910023556,0.12231914413229394),(-0.32705115386597394,0.6744065707101621),(0.2723211656942368,-0.8053758131859647)}";
