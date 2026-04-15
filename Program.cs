bc32eb0bc8550796f013d3e377a32d08bfb68f2f
Pratham Khanal <pkprathamkhanal@gmail.com>
2026-04-15 16:09:05 -0500

    Update Program.cs
    Enable memory caching by adding AddMemoryCache() in Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Configuration.GetSection("QuantumSolver").Get<QuantumSolverSettings>();
 builder.Services.AddControllers();
+builder.Services.AddMemoryCache(); //AddMemoryCache because without registering it, the backend won't inject it properly. 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
