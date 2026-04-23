e7ccb8e5471c046c2749ab38875474500aff9ac3
kramandr <andreaskramer@isu.edu>
2026-04-23 11:39:24 -1000

    Added Prim's Solver and tests

--- redux-tests/Problems/P_MINIMUMSPANNINGTREE/MINIMUMSPANNINGTREE_Tests.cs
+++ redux-tests/Problems/P_MINIMUMSPANNINGTREE/MINIMUMSPANNINGTREE_Tests.cs
@@ -28,11 +28,30 @@ public class MINIMUMSPANNINGTREE_Tests
     }
 
     [Fact]
+    public void MINIMUMSPANNINGTREE_PrimSolver_Simple_Triangle()
+    {
+        string instance = "({1,2,3},{({1,2},1),({2,3},2),({1,3},3)})";
+        var problem = new P_MINIMUMSPANNINGTREE(instance);
+        string solution = new PrimSolver().solve(problem);
+        Assert.Equal("{{1,2},{2,3}}", solution);
+    }
+
+    [Fact]
     public void MINIMUMSPANNINGTREE_Verifier_Accepts_Optimal_Triangle()
     {
         string instance = "({1,2,3},{({1,2},1),({2,3},2),({1,3},3)})";
         var problem = new P_MINIMUMSPANNINGTREE(instance);
         bool result = new MinimumSpanningTreeVerifier().verify(problem, "{{1,2},{2,3}}");
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void MINIMUMSPANNINGTREE_Verifier_Accepts_PrimSolver_Certificate()
+    {
+        string instance = "({1,2,3,4},{({1,2},1),({2,3},2),({3,4},1),({1,4},2),({1,3},3)})";
+        var problem = new P_MINIMUMSPANNINGTREE(instance);
+        string solution = new PrimSolver().solve(problem);
+        bool result = new MinimumSpanningTreeVerifier().verify(problem, solution);
         Assert.True(result);
     }
 
@@ -103,6 +122,15 @@ public class MINIMUMSPANNINGTREE_Tests
     }
 
     [Fact]
+    public void MINIMUMSPANNINGTREE_PrimSolver_Handles_Equal_Weight_Edges()
+    {
+        string instance = "({1,2,3,4},{({1,2},1),({2,3},1),({3,4},1),({1,4},1),({2,4},1)})";
+        var problem = new P_MINIMUMSPANNINGTREE(instance);
+        string solution = new PrimSolver().solve(problem);
+        Assert.Equal(3, GraphParser.parseUndirectedEdgeListWithStringFunctions(solution).Count / 2);
+    }
+
+    [Fact]
     public void MINIMUMSPANNINGTREE_Solver_Supports_Single_Vertex()
     {
         string instance = "({1},{})";
@@ -112,11 +140,29 @@ public class MINIMUMSPANNINGTREE_Tests
     }
 
     [Fact]
+    public void MINIMUMSPANNINGTREE_PrimSolver_Supports_Single_Vertex()
+    {
+        string instance = "({1},{})";
+        var problem = new P_MINIMUMSPANNINGTREE(instance);
+        string solution = new PrimSolver().solve(problem);
+        Assert.Equal("{}", solution);
+    }
+
+    [Fact]
     public void MINIMUMSPANNINGTREE_Solver_Returns_Empty_For_Disconnected_Graph()
     {
         string instance = "({1,2,3},{({1,2},1)})";
         var problem = new P_MINIMUMSPANNINGTREE(instance);
         string solution = new KruskalSolver().solve(problem);
+        Assert.Equal("{}", solution);
+    }
+
+    [Fact]
+    public void MINIMUMSPANNINGTREE_PrimSolver_Returns_Empty_For_Disconnected_Graph()
+    {
+        string instance = "({1,2,3},{({1,2},1)})";
+        var problem = new P_MINIMUMSPANNINGTREE(instance);
+        string solution = new PrimSolver().solve(problem);
         Assert.Equal("{}", solution);
     }
 }
