bdb855f37d3210cbfeb40bc3356645c0f417de53
RajNilkar <rajitnilkar28@gmail.com>
2026-04-27 17:04:50 -0600

    Added new feature for step-viz
    Tracks the current node in a different color when going through each step

--- Problems/NPComplete/NPC_SHORTESTPATH/Visualizations/ShortestPathVisualization.cs
+++ Problems/NPComplete/NPC_SHORTESTPATH/Visualizations/ShortestPathVisualization.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using API.Interfaces;
+﻿using API.Interfaces;
 using API.Interfaces.Graphs.GraphParser;
 using API.Interfaces.JSON_Objects;
 using API.Interfaces.JSON_Objects.Graphs;
+using System;
+using System.Collections.Generic;
 
 namespace API.Problems.NPComplete.NPC_SHORTESTPATH.Visualizations;
 
@@ -75,8 +75,61 @@ class ShortestPathVisualization : IVisualization<SHORTESTPATH>
 
         foreach (var step in steps)
         {
-            // reuse existing logic for highlighting a path
-            result.Add(SolvedVisualization(problem, step));
+            if(string.IsNullOrWhiteSpace(step) || step.Trim() == "{}")
+            {
+                result.Add(visualize(problem));
+            }
+
+            List<string> path;
+            try
+            {
+                path = GraphParser.parseNodeListWithStringFunctions(step);
+            }
+            catch
+            {
+                result.Add(visualize(problem));
+                continue;
+            }
+
+            API_GraphJSON graph = problem.graph.ToAPIGraph();
+
+            string currentNode = path.Count > 0 ? path[path.Count - 1] : null;
+            var pathNodes = new HashSet<string>(path);
+
+            for(int i = 0; i < graph.nodes.Count; i++)
+            {
+                if (graph.nodes[i].name == currentNode)
+                {
+                    graph.nodes[i].color = "ElementHighlight";
+                    graph.nodes[i].outline = "Purple";
+                }
+                else if (pathNodes.Contains(graph.nodes[i].name))
+                {
+                    graph.nodes[i].color = "Solution";
+                }
+                else
+                {
+                    graph.nodes[i].color = "Background";
+                }
+            }
+
+            var pathEdges = new HashSet<(string u, string v)>();
+            for (int i = 0; i < path.Count - 1; i++)
+                pathEdges.Add((path[i], path[i + 1]));
+
+            for(int i = 0; i < graph.links.Count; i++)
+            {
+                var link = graph.links[i];
+                bool isForwardPathEdge = pathEdges.Contains((link.source, link.target));
+                bool isReversePathEdge = !problem.isDirected && pathEdges.Contains((link.target, link.source));
+
+                if (isForwardPathEdge || isReversePathEdge)
+                    link.color = "Solution";
+                else
+                    link.color = "Background";
+            }
+
+            result.Add(graph);
         }
 
         return result;
