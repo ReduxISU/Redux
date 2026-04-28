97708b490c7ac824a95920d46b849bb35227380b
dfawson <dannyfawson@gmail.com>
2026-04-28 16:47:05 -0600

    Added SUDOKU_Tests

---
+++ redux-tests/Problems/NPC_SUDOKU/SUDOKU_Tests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace redux_tests;
+#pragma warning disable CS1591
+
+public class ExampleTest
+{
+    [Fact]
+    public void exampleAddTest()
+    {
+        Assert.Equal(3, (1 + 2));
+    }
+}
