32828e38286a0de22fa138a9e65fc35c7f5c1054
Kaosi Ibeabuchi <kaosiibeabuchi@isu.edu>
2026-04-30 08:17:30 -0600

    Updates correct contributors for EditDistanceVerifier

--- Problems/NPComplete/NPC_EDITDISTANCE/Verifiers/EditDistanceVerifier.cs
+++ Problems/NPComplete/NPC_EDITDISTANCE/Verifiers/EditDistanceVerifier.cs
@@ -12,7 +12,7 @@ class EditDistanceVerifier : IVerifier<EDITDISTANCE> {
     public string verifierName { get; } = "Edit Distance Verifier";
     public string verifierDefinition { get; } = "Verifies that a proposed edit distance is correct for the input strings.";
     public string source { get; } = "https://en.wikipedia.org/wiki/Edit_distance";
-    public string[] contributors { get; } = { "Andreas Kramer", "Val Kimbrough" };
+    public string[] contributors { get; } = { "Kaosi Ibeabuchi", "Diya Pandey", "Srijan Pant" };
     private string _certificate = string.Empty;
     public string certificate => _certificate;
 
